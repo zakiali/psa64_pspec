d7fd1473cc2806e67c519e653b38c5a8dbfa7500
Zaki Ali <zakiali88@gmail.com>
2014-11-23 16:01:28 -0800

    add in waterfall plots and script for making them

---
+++ plots/plot_waterfall_fg.sh
@@ -0,0 +1,7 @@
+plot_uv_zaki.py -a 1_4 --time_axis=lst -C psa6240_v003 -p I --out_file=foreground_lst_bl --max=3.8 --drng=4.8 /Users/sherlock/projects/paper/analysis/psa64/foregrounds/*.uvA
+
+plot_uv_zaki.py -a 1_4 --time_axis=lst -C psa6240_v003 -p I --out_file=wbd_lst_bl --max=3.8 --drng=4.8 /Users/sherlock/projects/paper/analysis/psa64/sep01/*.uvA 
+
+plot_uv_zaki.py -a 1_4 --time_axis=lst -C psa6240_v003 -p I --out_file=frf_lst_bl --max=3.8 --drng=4.8 /Users/sherlock/projects/paper/analysis/psa64/sep01/*.uvL 
+
+
